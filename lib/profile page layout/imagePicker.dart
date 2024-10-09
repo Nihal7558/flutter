f3f4331cfdb6d3826466b8a287baa9544d56b68a
Nihal7558 <mnihalep345@gmail.com>
2024-10-09 10:00:35 +0530

    Daily Task

--- lib/profile page layout/imagePicker.dart
+++ lib/profile page layout/imagePicker.dart
@@ -26,62 +26,88 @@ class _ImagepickerExState extends State<ImagepickerEx> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CircleAvatar(
-              child: _image == null || kIsWeb
-                  ? Image.asset("assets/IMG_6276.JPG")
-                  : Image.file(_image!),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text("Muhammad Nihal"),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (_) {
-                        return AlertDialog(
-                          title: Text('Select source of the image'),
-                          content: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                    _pickImage(ImageSource.camera);
-                                  },
-                                  child: Text('Camera')),
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                    _pickImage(ImageSource.gallery);
-                                  },
-                                  child: Text('Gallery')),
-                            ],
-                          ),
-                        );
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AppBar(
+                title: Text(
+                  "Profile",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Colors.teal,
+              ),
+              SizedBox(
+                height: 48,
+              ),
+              CircleAvatar(
+                radius: 40,
+                backgroundImage: _image == null || kIsWeb
+                    ? AssetImage("assets/IMG_6276.JPG")
+                    : FileImage(_image!),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Muhammad Nihal"),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lime[500]),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (_) {
+                          return AlertDialog(
+                            title: Text(
+                              'Select source of the image',
+                            ),
+                            content: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.lime),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                      _pickImage(ImageSource.camera);
+                                    },
+                                    child: Text('Camera')),
+                                ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.lime),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                      _pickImage(ImageSource.gallery);
+                                    },
+                                    child: Text('Gallery')),
+                              ],
+                            ),
+                          );
+                        });
+                  },
+                  child: Text("click to change image")),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 185,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple[200]),
+                    onPressed: () {
+                      setState(() {
+                        _image = null;
                       });
-                },
-                child: Text("click to change image")),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _image = null;
-                  });
-                },
-                child: Text('clear image'))
-          ],
+                    },
+                    child: Text('clear image')),
+              )
+            ],
+          ),
         ),
       ),
     );
